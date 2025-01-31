5811fd7949116f3ca3cfda2b37bcbd1366dc6f76
Ardione321 <ardioned@gmail.com>
2025-01-31 12:20:45 +0800

    Add theme provider for dark, light and system mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import { Inter, Barlow } from "next/font/google";
 // Global css
 import "./globals.css";
 
+// Theme provider
+import { ThemeProvider } from "next-themes";
+
 //Fonts
 const interFont = Inter({ subsets: ["latin"] });
 const barlowFont = Barlow({
@@ -12,6 +15,7 @@ const barlowFont = Barlow({
   weight: ["500", "700"],
   variable: "--font-barlow",
 });
+
 // Metadata
 export const metadata: Metadata = {
   title: "SuzyShop",
@@ -25,11 +29,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${interFont.className} ${barlowFont.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import ThemeToogle from "@/components/shared/theme-toogle";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+      <ThemeToogle />
+      </div>
       <h1 className="text-blue-500 font-barlow">Welcome to the ecommerce</h1>
       <Button variant="destructive">Click here</Button>
     </div>
